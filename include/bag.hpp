2c84b785277615c75bdcd74d5c83640d3b8e8f7b
Clucch <mattiacerutti04@gmail.com>
2024-05-07 21:40:09 +0200

    Add getters, setters and constructors

--- include/bag.hpp
+++ include/bag.hpp
@@ -1,5 +1,54 @@
+
+#include <vector>
+
 /*
  * Your implementation of the container bag<Val>.
  * Write both the declaration and definition of the
  * container in this file.
  */
+
+template <typename T>
+struct bag {
+
+    //Contructors and deconstructor
+    
+    bag() {
+        nodes = vector<T>();
+    }
+
+    ~bag() {
+        nodes.clear();
+    }
+
+    //Methods
+    void add(T node) {
+        for(int i = 0; i < nodes.size(); i++){
+
+            T currentNode = nodes[i];
+
+            if(node.get_label() > currentNode.get_label() ){
+                if(i == nodes.size() - 1){
+                    nodes.push_back(node);
+                    return;
+                }
+
+                if(node.get_label() < nodes[i+1].get_label()){
+                    nodes.insert(nodes.begin() + i + 1, node);
+                    return;
+                }
+            } else {
+                nodes.insert(nodes.begin() + i, node);
+                return;
+            }
+        }
+    }
+
+
+
+
+    private:
+
+    //Data
+    vector<T> nodes;
+
+};
--- src/trie.cpp
+++ src/trie.cpp
@@ -1,3 +1,84 @@
 #include "trie.hpp"  // It is forbidden to include other libraries!
 
 /* Here below, your implementation of trie. */
+template <typename T>
+void trie<T>::set_weight(double w) {
+
+    /* TODO: Implement leaf check: if the node IS NOT a leaf (meaning it has some children) should throw an error*/
+
+
+    this->m_w = w;
+}
+
+template <typename T>
+double trie<T>::get_weight() const {
+    return this->m_w;
+}
+
+template <typename T>
+void trie<T>::set_label(const T* l) {
+
+    // Set the label only if node IS NOT the root (meaning it has a parent)
+    if(this->m_p){
+
+        /* TODO: Implement label check on parent children: label should be assigned on this node only if there is not another arc from a parent to one of the siblings with the same label*/
+
+        this->m_l = l;
+    }
+
+    
+}
+
+template <typename T>
+T const* trie<T>::get_label() const {
+
+    // Return the label only if node IS NOT the root (meaning it has a parent)
+    if(this->m_p){
+        return this->m_l;
+    }
+    //TODO: Throw error
+
+}
+
+template <typename T>
+void trie<T>::set_parent(const trie<T>* p){   
+    this->m_p = p;
+}
+
+template <typename T>
+const trie<T>* trie<T>::get_parent() const {
+    return this->m_p;
+}
+
+template <typename T>
+void trie<T>::add_child(trie<T> const& c){
+
+    /* C should have a valid label before being added*/
+
+    if(c.get_label() == nullptr){
+        //FIXME: Throw error? Idk
+    }
+
+    this->m_c.add(c);
+}
+
+template <typename T>
+bag<trie<T>> const& trie<T>::get_children() const{
+    return this->m_c;
+}
+
+template <typename T>
+trie<T>::trie(){
+    this->m_p = nullptr;
+    this->m_l = nullptr;
+    this->m_w = 0.0;
+}
+
+template <typename T>
+trie<T>::trie(double w){
+    this->m_p = nullptr;
+    this->m_l = nullptr;
+    this->m_w = w;
+}
+
+
